a32c95c8dfedae105a5a7e905c946bdcf720f0e4
Wairagu Helen <hwangari3@gmail.com>
2019-02-21 12:48:51 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <app-Posts v-bind:Posts="Posts"></app-Posts>
+  </div>
+</template>
+
+<script>
+import Posts from './Posts';
+export default {
+  name: 'App',
+  components: {
+    'app-Posts':Posts
+  },
+  data () {
+    return {
+        Posts: [
+         {title: "My journey with Vue"},
+         {title: "I love Vue"},
+         {title: "Vue"}
+        ]
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,30 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+<template>
+  <div id = "Posts">
+      <ul>
+          <li v-for="Post in Posts" :key="Post">
+              <h1>{{Post.title}}</h1>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {Posts:
+     {
+type:Array
+    }
+    },
+  data () {
+    return {
+        
+  }
+}
+}
+</script>
+
